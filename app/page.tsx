5cf580b1ad8e91c54d8f2c673fb4380244380bed
Cryyufstal <86175191+Cryyufstal@users.noreply.github.com>
2025-01-02 13:29:58 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@
 import WebApp from "@twa-dev/sdk";
 import { useEffect, useState } from "react";
 // pages/_app.tsx
-import 'global.css'; // مسار ملف CSS
+import '/global.css'; // مسار ملف CSS
 import type { AppProps } from 'next/app';
 
 export default function App({ Component, pageProps }: AppProps) {
