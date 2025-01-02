887cbbf4b569ade4c2286ff23bf6a60eb458787b
Cryyufstal <86175191+Cryyufstal@users.noreply.github.com>
2025-01-02 01:30:29 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -26,14 +26,9 @@ export default function Home() {
     <main className="p-4">
       {userData ? (
         <>
-          <h1 className="text-2xl font-bold mb-4">User Data</h1>
+          <h1 className="text-2xl font-bold mb-4">free style bot</h1>
           <ul>
-            <li>ID: {userData.id}</li>
-            <li>First Name: {userData.first_name}</li>
-            <li>Last Name: {userData.last_name || 'N/A'}</li>
-            <li>Username: {userData.username || 'N/A'}</li>
-            <li>Language Code: {userData.language_code}</li>
-            <li>Is Premium: {userData.is_premium ? 'Yes' : 'No'}</li>
+            <li>hello: {userData.username}</li>
           </ul>
         </>
       ) : (
